331448e00306bf61c3e8c6c08534def901a480ad
namasayayusi <yusufislamsalims@gmail.com>
2020-07-10 11:30:55 +0700

    update registrasi

--- src/pages/registrasi/styles.js
+++ src/pages/registrasi/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles(theme => ({
   daftarBlock: {
     width: "100%",
-    height: "100vh",
+    height: "100%",
     backgroundColor: "#005f80",
   },
   daftarBox: {
